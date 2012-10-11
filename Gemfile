18a19df5329efa5fd2b6d56a8e1f31d7bcdf320d
Robert <kolbobby@gmail.com>
2012-10-10 20:39:19 -0700

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.3'
 group :development, :test do
   gem 'sqlite3', '1.3.6'
   gem 'rspec-rails', '2.9.0'
+  gem 'guard-rspec', '0.5.5'
 end
 
 gem 'json'
@@ -27,7 +28,12 @@ end
 gem 'jquery-rails', '2.1.3'
 
 group :test do
+  gem 'rspec-rails', '2.9.0'
   gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 
 group :development do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Sample App" }
+  
   describe "HomePage" do
     it "should have the correct title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => " | Home")
     end
     it "should have the h1 content 'Sample App'" do
       visit '/static_pages/home'
@@ -15,7 +21,7 @@ describe "StaticPages" do
   describe "HelpPage" do
     it "should have the correct title" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => 'Sample App | Help')
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
     it "should have the h1 content 'Help'" do
       visit '/static_pages/help'
@@ -26,7 +32,7 @@ describe "StaticPages" do
   describe "AboutPage" do
     it "should have the correct title" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => 'Sample App | About Us')
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
     it "should have the h1 content 'About Us'" do
       visit '/static_pages/about'
